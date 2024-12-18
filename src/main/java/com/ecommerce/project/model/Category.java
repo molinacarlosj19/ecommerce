dbce687b247a61d88ca8efcd762e4f5e7a869d92
carlos molina <molinacarlosj@gmail.com>
2024-12-17 21:43:48 -0500

    Adding Repository for Category

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,7 +1,21 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.data.annotation.Version;
+
+@Entity(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
+
+    @Version
+    private Integer version;
+
     private String categoryName;
 
     public Category() {
